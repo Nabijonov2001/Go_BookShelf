b5054dab080b6d646dd185a5066f8718eb6ace61
fazliddin2001 <fazliddinnabijonov6@gmail.com>
2022-10-16 07:47:50 +0500

    changed

--- service/models/user.go
+++ service/models/user.go
@@ -3,11 +3,11 @@ package models
 type User struct {
 	ID     uint   `gorm:"primaryKey" json:"id"`
 	Name   string `json:"name" binding:"required,gte=3,lte=30"`
-	Key    string `gorm:"unique" json:"key" binding:"required,gte=4,lte=10"`
+	Key    string `gorm:"unique" json:"key" binding:"required,gte=3,lte=20"`
 	Secret string `json:"secret" binding:"required,gte=4,lte=10"`
 }
 
 type UserOne struct {
-	Key  string `json:"key"  binding:"required,gte=4,lte=10"`
+	Key  string `json:"key"  binding:"required,gte=3,lte=20"`
 	Sign string `json:"sign"  binding:"required"`
 }
